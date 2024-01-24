38d6eabd67ff20b18d48691f77445a981c863a3f
abinayam09 <abinayamabu@gmail.com>
2024-01-24 12:39:38 +0530

    code added

---
+++ src/com/test/GitClass1.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class GitClass1 {
+private void course() {
+	// TODO Auto-generated method stub
+System.out.println("Java");
+}
+public static void main(String[] args) {
+	GitClass1 g = new GitClass1();
+	g.course();
+}
+}
