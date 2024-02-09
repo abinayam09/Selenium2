12ea99b5f946a00c627872edf1520706190ad8d7
abinayam09 <abinayamabu@gmail.com>
2024-02-09 13:10:43 +0530

    login done by story1

--- src/com/test/GitClass1.java
+++ src/com/test/GitClass1.java
@@ -5,8 +5,12 @@ private void course() {
 	// TODO Auto-generated method stub
 System.out.println("Java");
 }
+private void login() {
+	System.out.println("login done by story 1");
+}
 public static void main(String[] args) {
 	GitClass1 g = new GitClass1();
 	g.course();
+	g.login();
 }
 }
