28c377e97f9867913053983bdefa84eaa71d32bb
abinayam09 <abinayamabu@gmail.com>
2024-02-09 13:28:29 +0530

    new change

--- src/com/test/GitClass1.java
+++ src/com/test/GitClass1.java
@@ -5,6 +5,10 @@ private void course() {
 	// TODO Auto-generated method stub
 System.out.println("Java");
 }
+private void product() {
+	// TODO Auto-generated method stub
+System.out.println("product add done by story 2");
+}
 public static void main(String[] args) {
 	GitClass1 g = new GitClass1();
 	g.course();
