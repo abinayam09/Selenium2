f7bc7966bf2371ece954ebfdf93720f40184e3dc
abinayam09 <abinayamabu@gmail.com>
2024-02-09 13:23:29 +0530

    product add done by story2

--- src/com/test/GitClass1.java
+++ src/com/test/GitClass1.java
@@ -5,12 +5,12 @@ private void course() {
 	// TODO Auto-generated method stub
 System.out.println("Java");
 }
-private void login() {
-	System.out.println("login done by story 1");
+private void productAdd() {
+	System.out.println("product addition done by story 2");
 }
 public static void main(String[] args) {
 	GitClass1 g = new GitClass1();
 	g.course();
-	g.login();
+	g.productAdd();
 }
 }
