ef8ccced297cbff0bb499a0ab74f0aef8473f889
abinayam09 <abinayamabu@gmail.com>
2024-02-10 10:10:49 +0530

    new code

--- src/com/test/GitClass1.java
+++ src/com/test/GitClass1.java
@@ -8,6 +8,10 @@ System.out.println("Java");
 private void productAdd() {
 	System.out.println("product addition done by story 2");
 }
+private void addTOCart() {
+	System.out.println("products added to acrt by story1");
+}
+
 public static void main(String[] args) {
 	GitClass1 g = new GitClass1();
 	g.course();
